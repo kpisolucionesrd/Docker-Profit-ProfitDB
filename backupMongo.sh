e79879ce62bd1e142d3e11b923c9a33ac75f8421
KPIsolucionesrd <45852083+kpisolucionesrd@users.noreply.github.com>
2019-08-26 10:36:54 -0400

    Update Backup.sh
    Corrigiendo el script.. el flag --out estaba colocado mal.

--- backupMongo.sh
+++ backupMongo.sh
@@ -2,5 +2,5 @@
 DIR=`date +%d-%m-%y-%T`
 DEST=/db_backups/$DIR
 mkdir $DEST
-mongodump --db --out CanalIndirecto $DEST
-mongodump --db --out CanalDirecto $DEST
+mongodump --db CanalIndirecto --out $DEST
+mongodump --db CanalDirecto --out $DEST
