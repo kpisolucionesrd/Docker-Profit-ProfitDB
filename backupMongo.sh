e80178c8b319d0eb16acec56a17791a51f8dbbb2
KPIsolucionesrd <45852083+kpisolucionesrd@users.noreply.github.com>
2019-08-26 09:44:06 -0400

    Update backupMongo.sh

--- backupMongo.sh
+++ backupMongo.sh
@@ -2,5 +2,5 @@
 DIR=`date +%d-%m-%y-%T`
 DEST=/db_backups/$DIR
 mkdir $DEST
-mongodump --db CanalIndirecto $DEST
-mongodump --db CanalDirecto $DEST
+mongodump --db --out CanalIndirecto $DEST
+mongodump --db --out CanalDirecto $DEST
